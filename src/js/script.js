95d1fbc2ff8688a190008c9464d8e11310cd70b4
SergB <sergey.bab7@gmail.com>
2025-05-28 23:59:04 +0300

    promo carousel start

--- src/js/script.js
+++ src/js/script.js
@@ -29,7 +29,7 @@ loadLanguage(savedLang);
 document.addEventListener("click", e => {
   if (e.target.matches(".mainmenu__lang-item_UA")) {
     for (let i of items) {
-        i.classList.remove('mainmenu__lang-item_active');
+      i.classList.remove('mainmenu__lang-item_active');
     }
     e.target.classList.add('mainmenu__lang-item_active');
     loadLanguage("ua");
@@ -38,7 +38,7 @@ document.addEventListener("click", e => {
 document.addEventListener("click", e => {
   if (e.target.matches(".mainmenu__lang-item_RU")) {
     for (let i of items) {
-        i.classList.remove('mainmenu__lang-item_active');
+      i.classList.remove('mainmenu__lang-item_active');
     }
     e.target.classList.add('mainmenu__lang-item_active');
     loadLanguage("ru");
@@ -47,7 +47,7 @@ document.addEventListener("click", e => {
 document.addEventListener("click", e => {
   if (e.target.matches(".mainmenu__lang-item_EN")) {
     for (let i of items) {
-        i.classList.remove('mainmenu__lang-item_active');
+      i.classList.remove('mainmenu__lang-item_active');
     }
     e.target.classList.add('mainmenu__lang-item_active');
     loadLanguage("en");
@@ -56,8 +56,8 @@ document.addEventListener("click", e => {
 
 // гамбургер-меню
 const overlay = document.querySelector('.overlay'),
-      mainmenu__body = document.querySelector('.mainmenu__body'),
-      hamburger = document.querySelector('.mainmenu__hamburger-block');
+  mainmenu__body = document.querySelector('.mainmenu__body'),
+  hamburger = document.querySelector('.mainmenu__hamburger-block');
 
 document.querySelector('.mainmenu__hamburger').addEventListener('click', () => {
   overlay.classList.toggle('not-visible');
@@ -73,5 +73,19 @@ document.querySelector('.mainmenu__hamburger').addEventListener('click', () => {
   } else {
     hamburger.classList.add('mainmenu__hamburger-active');
   }
-  
+
 });
+
+// главное меню
+const mainMenu = document.querySelectorAll('.mainmenu__list_item');
+
+mainMenu.forEach(item => {
+  item.addEventListener('click', (item) => {
+    mainMenu.forEach(it => {
+      it.classList.remove('mainmenu__list_item-active');
+    });
+    item.target.classList.add('mainmenu__list_item-active');
+  });
+});
+
+// слайдер на главной странице (promo)
